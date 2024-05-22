6f9b65af88b63440a15ee461c62c64952a267c16
leo12wb <leonardo502013@gmail.com>
2024-05-22 14:24:05 -0300

    fix: ajuste

--- main.go
+++ main.go
@@ -3,62 +3,92 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-type Address struct {
-	CEP         string `json:"cep"`
-	State       string `json:"state"`
-	City        string `json:"city"`
-	Neighborhood string `json:"neighborhood"`
-	Street      string `json:"street"`
-	Service     string `json:"service"`
+type APIResponse interface{}
+
+type ApiResponse struct {
+	Api string `json:"Api"`
 }
 
-func requestAPI(url string, ch chan Address, source string) {
-	client := http.Client{Timeout: 1 * time.Second}
-	resp, err := client.Get(url)
-	if err != nil {
-		ch <- Address{CEP: "", Service: source}
-		return
-	}
-	defer resp.Body.Close()
+type ViaCEP struct {
+	ApiResponse
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
-	var address Address
-	err = json.NewDecoder(resp.Body).Decode(&address)
-	if err != nil {
-		ch <- Address{CEP: "", Service: source}
-		return
-	}
-
-	ch <- address
+type ApiCEP struct {
+	ApiResponse
+	Code       string `json:"code"`
+	State      string `json:"state"`
+	City       string `json:"city"`
+	District   string `json:"district"`
+	Address    string `json:"address"`
+	Status     int    `json:"status"`
+	Ok         bool   `json:"ok"`
+	StatusText string `json:"statusText"`
 }
 
 func main() {
-	cep := "01153000"
-	api1 := fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
-	api2 := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep)
+	channelViaCEP := make(chan ViaCEP)
+	channelApiCEP := make(chan ApiCEP)
 
-	ch := make(chan Address, 2)
-
-	go requestAPI(api1, ch, "BrasilAPI")
-	go requestAPI(api2, ch, "ViaCEP")
+	go GetViaCEP(channelViaCEP)
+	go GetApiCEP(channelApiCEP)
 
 	select {
-	case address := <-ch:
-		if address.CEP == "" {
-			fmt.Printf("Erro ao obter dados do endereço da %s\n", address.Service)
-			return
-		}
+	case resViaCEP := <-channelViaCEP:
+		fmt.Printf("ViaCEP: %+v\n", resViaCEP)
 
-		fmt.Printf("Dados do endereço obtidos pela API que respondeu mais rápido (%s):\n", address.Service)
-		fmt.Printf("CEP: %s\n", address.CEP)
-		fmt.Printf("Estado: %s\n", address.State)
-		fmt.Printf("Cidade: %s\n", address.City)
-		fmt.Printf("Bairro: %s\n", address.Neighborhood)
-		fmt.Printf("Rua: %s\n", address.Street)
-	case <-time.After(1 * time.Second):
-		fmt.Println("Timeout ao tentar obter os dados do endereço.")
+	case resApiCEP := <-channelApiCEP:
+		fmt.Printf("ApiCEP: %+v\n", resApiCEP)
+
+	case <-time.After(time.Second):
+		fmt.Printf("TimeOut")
+
 	}
+
+}
+
+func GetViaCEP(chApi chan ViaCEP) {
+	var viaCEP ViaCEP
+	RequestAPI("https://viacep.com.br/ws/22621252/json/", &viaCEP)
+	viaCEP.Api = "ViaCEP"
+	chApi <- viaCEP
+}
+
+func GetApiCEP(chVia chan ApiCEP) {
+	var apiCEP ApiCEP
+	RequestAPI("https://cdn.apicep.com/file/apicep/22260-003.json", &apiCEP)
+	apiCEP.Api = "ApiCEP"
+	chVia <- apiCEP
+}
+
+func RequestAPI(url string, res APIResponse) error {
+	req, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, res)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
